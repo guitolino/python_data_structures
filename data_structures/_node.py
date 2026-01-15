53f6ba1fb3f52ddca11c7f6dc347447448568684
guitolino <migantp25@gmail.com>
2026-01-14 21:28:16 -0500

    feat(_node.py): create internal class _Node

---
+++ data_structures/_node.py
@@ -0,0 +1,8 @@
+class _Node():
+    __slots__ = ("value", "next")
+    def __init__(self, value: any, next: _Node | None):
+        self.value: any = value
+        self.next: _Node | None = next
+    
+    def __repr__(self):
+        return f"value={self.value}\nnext={self.next.value}"
