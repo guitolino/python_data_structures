57957b99a45ec21880320248297de74c00d3c6c4
guitolino <migantp25@gmail.com>
2026-01-15 23:15:21 -0500

    feat(data_structures): add pop() function in class Stack

--- data_structures/stack.py
+++ data_structures/stack.py
@@ -1,5 +1,6 @@
 from ._node import _Node
 
+
 class Stack:
     __slot__ = ("__last_node", "__length")
     def __init__(self):
@@ -12,6 +13,14 @@ class Stack:
         self.__last_node = new_node
         self.__length += 1
     
+    def pop(self):
+        if not self.__last_node:
+            return None
+        
+        aux = self.last_value
+        self.__last_node = self.__last_node.next
+        return aux
+
     @property
     def last_value(self) -> any | None:
         return self.__last_node.value if self.__last_node else None
@@ -19,3 +28,5 @@ class Stack:
     @property
     def length(self) -> int:
         return self.__length
+    
+    # --- public API ---
