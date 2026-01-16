124b15e75d26e65205391bd51a1935f60507d999
guitolino <migantp25@gmail.com>
2026-01-16 09:14:11 -0500

    test: add tests for Stack's `is_empty` property

--- tests/test_.py
+++ tests/test_.py
@@ -13,8 +13,16 @@ class TestStack:
         ((4,53,23,121,23), 23,),
         ((), None,),
     ])
-    def test_push(self, insertions, last_value):
+    def test_push_last_value_length(self, insertions, last_value):
         for i in insertions:
             self.stack.push(i)
         assert self.stack.last_value == last_value
         assert self.stack.length == len(insertions)
+    
+   
+    def test_is_empty_true(self):
+        assert self.stack.is_empty
+    
+    def test_is_empty_false(self):
+        self.stack.push(10)
+        assert not self.stack.is_empty
