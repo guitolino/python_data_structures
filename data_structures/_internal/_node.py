57c7ded1e68ce9d04ae8d26128f3d752cde6fb55
guitolino <migantp25@gmail.com>
2026-01-16 07:40:40 -0500

    refactor: create a new subpackage _internal/ and move _node.py inside
    - create _internal/ subpackage for private/internal files and logic
    - move _node.py into _internal/

--- data_structures/_internal/_node.py
+++ data_structures/_internal/_node.py
@@ -1,8 +1,10 @@
+from typing import Any
+
 class _Node():
     __slots__ = ("value", "next")
-    def __init__(self, value: any, next: _Node | None):
-        self.value: any = value
+    def __init__(self, value: Any, next: _Node | None) -> None:
+        self.value: Any = value
         self.next: _Node | None = next
     
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"value={self.value}\nnext={self.next.value if self.next else None}"
