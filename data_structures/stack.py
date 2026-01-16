422070a22362800136de509cecee3bc9d03485b9
guitolino <migantp25@gmail.com>
2026-01-16 08:01:53 -0500

    chore(data_structures): imporve correct type hinting
    - use typing.Any for variables and functions expecting any type of value

--- data_structures/stack.py
+++ data_structures/stack.py
@@ -1,28 +1,29 @@
-from ._node import _Node
+from typing import Any
+from ._internal._node import _Node
 
 
 class Stack:
     __slot__ = ("__last_node", "__length")
-    def __init__(self):
+    def __init__(self) -> None:
         self.__last_node: _Node | None = None
         self.__length: int = 0
     
     # --- public API ---
-    def push(self, value: any):
+    def push(self, value: Any) -> None:
         new_node = _Node(value, self.__last_node)
         self.__last_node = new_node
         self.__length += 1
     
-    def pop(self):
+    def pop(self) -> Any | None:
         if not self.__last_node:
             return None
         
-        aux = self.last_value
+        aux: Any = self.last_value
         self.__last_node = self.__last_node.next
         return aux
 
     @property
-    def last_value(self) -> any | None:
+    def last_value(self) -> Any | None:
         return self.__last_node.value if self.__last_node else None
 
     @property
